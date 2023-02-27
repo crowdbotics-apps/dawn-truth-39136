bf9fdde2f5545cdfbddbac777a3b8d410475b869
Crowdbotics <team@crowdbotics.com>
2023-02-27 20:04:32 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dawn_truth_39136/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dawn_truth_39136;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "dawn_truth_39136";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dawn-truth-39136",
+  url: "https://dawn-truth-39136.botics.co",
+  api: "https://dawn-truth-39136.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
